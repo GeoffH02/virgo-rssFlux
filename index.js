7f802c423d023f9d742256669d37e88e6f1e1400
geoffrey <geoffrey.hallier@epitech.eu>
2022-07-20 14:00:33 +0200

    add with json

--- index.js
+++ index.js
@@ -1,8 +1,39 @@
 const express = require('express')
+const sqlite = require('sqlite3')
 const app = express()
-const port = 8000
-require("./models/dbConfig")
+const path = require("path")
+const port = 5655
+const dbName = 'virgo_api.db'
+
+app.use(express.static(path.join(__dirname, "public")))
+app.use(express.static(path.join(__dirname, "public/scripts")))
+
+let db = new sqlite.Database(dbName, err => {
+    if (err) throw err
+/*
+    db.run('CREATE TABLE news(name VARCHAR(255))')
+*/
+    db.run('INSERT INTO news VALUES(?)',['Teste'])
+
+
+/*    db.get('SELECT * FROM news', (err,data) =>{
+        if (err) throw err
+        console.log(data)
+    })*/
+
+    console.log("Base de donées op")
+})
 
 app.listen(port, () => {
-    console.log("Server Started :\n","https://localhost:"+port)
+    console.log("Server Started :\n","http://localhost:"+port +"/")
+})
+
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html")
+})
+
+db.close(err => {
+    if (err) throw  err
+    console.log("Base de données fermé")
 })
--- public/scripts/rss.js
+++ public/scripts/rss.js
@@ -1,7 +1,39 @@
 window.onload = () => {
     let coindesk = "https://www.coindesk.com/arc/outboundfeeds/rss/?outputType=xml"
     let coinnews = "https://www.coinnews.fr/feed"
+    let arrNews = []
     let content = document.querySelector(".content")
+
+    let jsonfile = {
+        "tokens": [{
+            "BTC": {
+                "id": 1,
+                "Articles": 3,
+                "lastFetch": 13.37,
+                "linkToArticles": []
+            },
+            "ETH": {
+                "id": 2,
+                "Articles": 3,
+                "lastFetch": 13.37,
+                "linkToArticles": []
+
+            },
+            "Virgo": {
+                "id": 3,
+                "Articles": 3,
+                "lastFetch": 13.37,
+                "linkToArticles": []
+            },
+            "NFT": {
+                "id": 3,
+                "Articles": 3,
+                "lastFetch": 13.37,
+                "linkToArticles": []
+            }
+        }]
+    };
+
 
     $.ajax({
         type: 'GET',
@@ -9,24 +41,44 @@ window.onload = () => {
         dataType: 'jsonp',
         success: function (result) {
             for (let i = 0; i < result.items.length; i++) {
-                console.log(result)
                 let insideDiv = document.createElement("div")
                 let paragraphe = document.createElement("p")
                 let createdAT = document.createElement("p")
                 let imageArticle = document.createElement("img")
                 let linktoBlog = document.createElement("a")
+
+
                 let image = imageArticle.src = result.items[i].enclosure.link;
-                createdAT.append(result.items[i].pubDate)
                 let articleLink = linktoBlog.href = result.items[i].link
                 let articleLinkText = linktoBlog.textContent = "Redirect"
-                imageArticle.width = 150
                 let title = `${result.items[i].title}`
-                paragraphe.append(title)
-                insideDiv.append(paragraphe)
-                insideDiv.append(createdAT)
-                insideDiv.append(linktoBlog)
-                insideDiv.append(imageArticle)
-                content.append(insideDiv)
+                let title_split = title.split(' ')
+
+                for (let i = 0; i < jsonfile.tokens.length; i++) {
+                    let obj = jsonfile.tokens[i];
+                    let objParse = Object.keys(obj)
+
+                    if (title_split.some(isin => objParse.includes(isin))){
+
+                        paragraphe.append(title)
+                        insideDiv.append(paragraphe)
+                        insideDiv.append(createdAT)
+                        insideDiv.append(linktoBlog)
+                        insideDiv.append(imageArticle)
+                        imageArticle.width = 150
+                        createdAT.append(result.items[i].pubDate)
+                        content.append(insideDiv)
+/*
+                        obj.linkToArticles = obj.linkToArticles + articleLink
+*/
+                        console.log(obj)
+                    }
+
+                }
+
+
+
+                /*console.log(title.split(' '))*/
             }
         }
     });
@@ -55,7 +107,6 @@ window.onload = () => {
                 insideDiv.append(createdAT)
                 insideDiv.append(linktoBlog)
                 insideDiv.append(imageArticle)
-                content.append(insideDiv)
             }
         }
     });
