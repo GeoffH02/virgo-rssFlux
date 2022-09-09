ef656142ba6563a67f84b7c4b937ceabd7581933
geoffrey <geoffrey.hallier@epitech.eu>
2022-09-09 16:53:05 +0200

    404 problem fixed, inserting tokens in DB succesfull

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const sqlite = require('sqlite3')
 const path = require("path")
 const app = express()
-const port = 3001
+const port = 3002
 const dbName = 'virgo_api.db'
 const {parse} = require('rss-to-json');
 
@@ -25,7 +25,7 @@ app.use(express.json());
 let db = new sqlite.Database(dbName, err => {
     if (err) throw err
     db.run('CREATE TABLE IF NOT EXISTS news(ticker,news_title,news_desc,news_img,news_link,pub_date)')
-    db.run('CREATE TABLE IF NOT EXISTS tokens(name,ticker,website,description,contract,decimals,CG_ID)')
+    db.run('CREATE TABLE IF NOT EXISTS tokens(name,ticker,website,description,contract,decimals,CG_ID,PRIMARY KEY (`contract`))')
 
 
 })
@@ -40,25 +40,22 @@ app.get('/emitNews', (req, res) => {
         .then(json => {
                 let fetchedData = json
 
-                for (const site of json) {
+                for (const site of fetchedData) {
                     console.log(site)
                     fetchedSite.push(site.url)
                     parse(site.url).then(rss => {
                         dataFlux.push(JSON.stringify(rss, null, 3))
-                        console.log(dataFlux.length)
+                        for (let i = 0; i < dataFlux.length; i++) {
+                            console.log(dataFlux[i])
+                        }
                     });
 
-                }
-
-                for (let i = 0; i < dataFlux.length; i++) {
-                    console.log(dataFlux)
                 }
             }
         )
 })
 
 app.get('/fetchNews', (req, res) => {
-
 
     let repoFetch = "https://raw.githubusercontent.com/virgoproject/tokens/main/infos.json"
     fetch(repoFetch)
@@ -74,16 +71,18 @@ app.get('/fetchNews', (req, res) => {
                     .then(jsonPArsed => jsonPArsed.json())
                     .then(json => {
                         for (const token of json.tokens) {
-                            if (individualChain !== "AVAX" || individualChain !== "CRO" || individualChain !== "AVAX" || individualChain !== "HT" || individualChain !== "KCS"   )
                                 fetch("https://raw.githubusercontent.com/virgoproject/tokens/main/" + individualChain + "/" + token + "/infos.json")
-                                    .then(response => response.json())
-                                    .then(data => {
-                                        console.log(data.name)
-                                        db.run('INSERT INTO tokens(name,ticker,website,description,contract,decimals,CG_ID) VALUES(?,?,?,?,?,?,?)',[data.name,data.ticker,data.website,data.description,data.contracts,data.decimals,data.CG_ID])
+                                    .then(response => {
+                                        if (response.ok){
+                                            response.json()
+                                                .then(data => {
+                                                    console.log(data)
+                                                    db.run('REPLACE INTO tokens(name,ticker,website,description,contract,decimals,CG_ID) VALUES(?,?,?,?,?,?,?)',[data.name,data.ticker,data.website,data.description,data.contract,data.decimals,data.CG_ID])
+                                                });
+                                        }
+                                    })
 
-                                    });
                         }
-
                     })
             }
         })
